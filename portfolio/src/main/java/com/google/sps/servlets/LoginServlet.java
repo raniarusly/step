5ad51e9deff0c3ab95d64814c3e3c40204a8da74
Rania Rusly <rania.rusly@gmail.com>
2020-07-27 19:11:27 +0000

    edit servlet to return login details

--- portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -16,21 +16,25 @@ package com.google.sps.servlets;
 
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.Gson;
+import com.google.sps.data.LoginDetails;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/user")
-public class UserServlet extends HttpServlet {
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html");
-
     UserService userService = UserServiceFactory.getUserService();
-    String userStatus = userService.isUserLoggedIn() ? "in" : "out";
-    response.getWriter().println("<p>Hello you are logged " + userStatus + "</p>");
+    boolean loginStatus = userService.isUserLoggedIn();
+    String loginUrl = userService.createLoginURL("contact.html");
+    LoginDetails loginDetails = new LoginDetails(loginStatus, loginUrl);
+
+    response.setContentType("application/json");
+    response.getWriter().println(new Gson().toJson(loginDetails));
   }
 }
