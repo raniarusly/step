38f94c2d1002ca823dba00539c1eae92526b7db2
Rania Rusly <rania.rusly@gmail.com>
2020-07-28 13:59:19 +0000

    show map in gallery page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -155,3 +155,10 @@ async function checkLogin(){
     document.getElementById("login-link").href = details.loginUrl;
   }
 }
+
+/** Creates a map and adds it to the page. */
+function initMap() {
+  const map = new google.maps.Map(
+      document.getElementById("map"),
+      {center: {lat: -6.175540, lng: 106.8274392}, zoom: 32});
+}
